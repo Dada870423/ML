0e76eb92c581208a720f68d094ba2a4daaf2bb11
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-10-06 13:46:57 +0800

    get L

--- MATRIX_OP.py
+++ MATRIX_OP.py
@@ -1,19 +1,34 @@
 import numpy as np
 
 class MATRIX_OP():
-    def iNV(self, input_matrix):
+    def iNvErSe(self, input_matrix):
         return ans
 
-    def lLT(self, input_matrix):
+    def LLT(self, input_matrix):
         return ans
 
     def getL(self, input_matrix):
         m, n = input_matrix.shape
         for iter_i in range(m - 1):
             for iter_k in range(1, m - iter_i):
-                print("first: ", input_matrix[iter_i + 1][iter_i], "second: ", input_matrix[iter_i][iter_i])
+                #print("first: ", input_matrix[iter_i + 1][iter_i], "second: ", input_matrix[iter_i][iter_i])
                 key = input_matrix[iter_i + iter_k][iter_i] / input_matrix[iter_i][iter_i]
-                for iter_j in range(iter_i, n):
-                	print(key)
-                	input_matrix[iter_i + iter_k][iter_j] = input_matrix[iter_i + iter_k][iter_j] - key * input_matrix[iter_i][iter_j]
-                print(input_matrix)
+                if key != 0:
+                    for iter_j in range(iter_i, n):
+                    	#print(key)
+                    	input_matrix[iter_i + iter_k][iter_j] = input_matrix[iter_i + iter_k][iter_j] - key * input_matrix[iter_i][iter_j]
+                    #print(input_matrix)
+        L = self.GetOutSigma(UUU = input_matrix)
+        print("L:\n", L)
+        return L
+    def GetOutSigma(self, UUU):
+        #print("GetOutSigma: ############")
+        m, n = UUU.shape
+        for iter_i in range(m):
+            key = UUU[iter_i][iter_i] ** 0.5
+            #print("key: ", key)
+            for iter_j in range(iter_i, n):
+                if key != 0:
+                    UUU[iter_i][iter_j] = UUU[iter_i][iter_j] / key
+        #print(UUU)
+        return UUU.T ## return L
