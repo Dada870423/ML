87aee2dd9b237e909e91cc054eb497f6e532c155
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-10-13 23:01:33 +0800

    no gauss jordan

--- hw1/MATRIX_OP.py
+++ hw1/MATRIX_OP.py
@@ -5,11 +5,33 @@ class MATRIX_OP():
     def iNvErSe(self, input_matrix):
         target_matrix = copy.deepcopy(input_matrix)
         LL = self.Cholesky(input_matrix = target_matrix)
-        Inv_L = self.Inv_l(LL = LL)
+        #print("LLLLL:", LL)
+        #Inv_L = self.Inv_l(LL = LL)
+        Inv_L = self.LyI(LL = LL)
+
+        
         Inv_U = Inv_L.T
         inv_input_matrix = Inv_U @ Inv_L
 
         return inv_input_matrix
+
+
+    def LyI(self, LL):
+        ## Ly = I, y = inverse of L
+        m, n = LL.shape
+        II = np.identity(n)
+        YY = np.identity(n)
+        for iter_i in range(n):
+            for iter_j in range(n):
+                tmp = II[iter_i][iter_j]
+                if LL[iter_i][iter_i] != 0:
+                    for iter_k in range(iter_i):
+                        tmp = tmp - (LL[iter_i][iter_k] * YY[iter_k][iter_j])
+                    YY[iter_i][iter_j] = (tmp) / LL[iter_i][iter_i]
+        #print("LL", LL)
+        #print("YY:", YY)
+        return YY
+
 
 
     def Inv_l(self, LL):
