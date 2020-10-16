6d60829313e1efb28303325cd26cee69ac5f90d0
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-10-16 13:51:27 +0800

    correct test

--- hw2/test_MNIST_CC.py
+++ hw2/test_MNIST_CC.py
@@ -2,7 +2,7 @@ from MNIST_CONTINUEOUS import MNIST_CONTINUEOUS
 
 MNIST_CC = MNIST_CONTINUEOUS()
 
-MNIST_CC.TRAIN("t10k-labels-idx1-ubyte", "t10k-images-idx3-ubyte")
+MNIST_CC.TRAIN("train-labels-idx1-ubyte", "train-images-idx3-ubyte")
 
 M, V, P = MNIST_CC.Get_MVP()
 
