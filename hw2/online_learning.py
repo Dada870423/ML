3230caeee94c768fd8115504f5bef95a7dd10619
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-10-18 23:06:03 +0800

    online learning init

---
+++ hw2/online_learning.py
@@ -0,0 +1,24 @@
+import sys
+import math
+import numpy as np
+class MNIST_CONTINUEOUS(objects):
+    def __init__(self, file):
+        self.read = False
+        self.file = file
+    #    self.train_label_file = train_label_file
+    #    self.test_image_file = test_image_file
+    #    self.test_label_file = test_label_file
+
+
+    def read_file():
+        if not self.read:
+            self.ptr = open(self.file, "r")
+            line = self.ptr.readline()
+        else:
+            line = self.ptr.readline()
+        return line
+
+    def Train()
+
+
+
