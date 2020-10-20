cfe9015f1433d16e0ed5b9753d69e3ca542afe0b
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-10-20 18:11:36 +0800

    bug fix

--- hw2/hw2_MNIST.py
+++ hw2/hw2_MNIST.py
@@ -15,7 +15,7 @@ if CD_mode != "Discrete":
 else:
     MNIST_DS = MNIST_DISCRETE()
     print("Discrete gogo!!")
-    MNIST_DS.TRAIN(train_label_file = "train-labels-idx1-ubyte", train_image_file = "file/train-images-idx3-ubyte")
+    MNIST_DS.TRAIN(train_label_file = "file/train-labels-idx1-ubyte", train_image_file = "file/train-images-idx3-ubyte")
     print("TRAIN done")
     MNIST_DS.Test(test_label_file = "file/t10k-labels-idx1-ubyte", test_image_file = "file/t10k-images-idx3-ubyte")
     for digit in range(10):
