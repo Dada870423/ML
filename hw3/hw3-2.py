2208f10e0e24b9ec383d9f5f0c30610767b93179
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-10-24 18:03:45 +0800

    hw3-2 done

---
+++ hw3/hw3-2.py
@@ -0,0 +1,19 @@
+import sys
+from gaussian import *
+
+
+
+mean = float(input("mean: "))
+varinance = float(input("varinance: "))
+
+print("Data point source function: N(", mean, ", ", varinance, ")")
+
+sourse = list()
+
+for i in range(10000):
+    SSSS = Univariate_generator(mean = mean, varinance = varinance)
+    print("Add data point: ", SSSS)
+    print("mean = ", np.mean(sourse), "   Variance = ", np.var(sourse))
+    sourse.append(SSSS)
+
+
