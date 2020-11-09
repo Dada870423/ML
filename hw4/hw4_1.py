9d2919caab077ece220fd8a603d102e6874f40c0
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-11-09 20:06:55 +0800

    clean code

--- hw4/hw4_1.py
+++ hw4/hw4_1.py
@@ -18,22 +18,19 @@ parser.add_argument("--vy2", type = float, default =  2.0, help = "vy2")
 
 input_ = parser.parse_args()
 
-
 data_para_1 = [input_.mx1, input_.vx1, input_.my1, input_.vy1]
 data_para_2 = [input_.mx2, input_.vx2, input_.my2, input_.vy2]
 
 ## hyperparameter
-LearningRating = 0.01
-
-
+LearningRate= 0.01
 
 
 sample_point, A, y = generage_point(data_para_1 = data_para_1, data_para_2 = data_para_2, points_num = input_.N)
 
 ### Gradient
 w = np.zeros(3)
-Gradient_result_w = Gradient_desent(w = w, A = A, y = y, lr = LearningRating)
 print("Gradient descent:\n\nw:\n")
+Gradient_result_w = Gradient_desent(w = w, A = A, y = y, lr = LearningRate)
 gradient_c0, gradient_c1 = predict(w = Gradient_result_w, A = A, sample_point = sample_point, points_num = input_.N)
 
 print("-" * 73)
@@ -41,7 +38,7 @@ print("-" * 73)
 ## Newton's
 w = np.zeros(3)
 print("Newton's method:\n\nw:\n")
-Newton_result_w = Newton(w = w, A = A, y = y, lr = LearningRating)
+Newton_result_w = Newton(w = w, A = A, y = y, lr = LearningRate)
 Newton_c0, Newton_c1 = predict(w = Newton_result_w, A = A, sample_point = sample_point, points_num = input_.N)
 
 ploting(sample_point = sample_point, gradient_c0 = gradient_c0, gradient_c1 = gradient_c1, \
