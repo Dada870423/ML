2446f14cec47b648523aa02b1d87d3bf6b1812c8
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-11-17 20:26:06 +0800

    make the code readable

--- hw4/EMEM.py
+++ hw4/EMEM.py
@@ -15,13 +15,9 @@ class EMEM(object):
 
 
     def E_step(self):
-        #print(self.lamBda)
-        #print((self.probability[3]))
-        #print((self.probability[4]))
         for iter_image in range(self.input_N):
             for iter_digit in range(10):
                 self.hidden_W[iter_image][iter_digit] = self.lamBda[iter_digit]
-                it = 0
                 for iter_pixel in range(28 * 28):
                     if self.Binomial_matrix[iter_image][iter_pixel] == 1:
                         self.hidden_W[iter_image][iter_digit] *= self.probability[iter_digit][iter_pixel]
@@ -31,19 +27,8 @@ class EMEM(object):
                     if iter_pixel % 10 == 0:
                         self.hidden_W[iter_image][iter_digit] *= self.jimmy
 
-                            
-
-                #print("in Estep")
-            #print(self.hidden_W[iter_image])
-            #if iter_image == 3:
-            #    print("before", self.hidden_W[3])
-            #if iter_image == 4:
-            #    print("before", self.hidden_W[4])
             self.hidden_W[iter_image] = norm_probability(self.hidden_W[iter_image])
         self.hidden_W[self.hidden_W<0.001] = 0.001
-        #print("after", self.hidden_W[3])
-        #print("after", self.hidden_W[4])
-
 
 
 
@@ -51,13 +36,10 @@ class EMEM(object):
 
 
     def M_step(self):
-        ## lamBda
-        #print(self.lamBda)
         for iter_digit in range(10):
             self.lamBda[iter_digit] = 0
             for iter_image in range(self.input_N):
                 self.lamBda[iter_digit] += self.hidden_W[iter_image][iter_digit]
-            #self.lamBda[iter_digit] = self.lamBda[iter_digit]
 
 
         for iter_digit in range(10):
@@ -69,16 +51,4 @@ class EMEM(object):
                             
                 self.probability[iter_digit][iter_pixel] /= self.lamBda[iter_digit]
         self.lamBda= norm_probability(self.lamBda)
-
-
-
-
-
-
-
-
-
-
-
-
 
