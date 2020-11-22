7175b0b79da13d00f6ec6942c7a7ee6cbf094115
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-11-22 19:01:50 +0800

    clean code

--- hw5/GaussianProcess.py
+++ hw5/GaussianProcess.py
@@ -10,7 +10,6 @@ class GaussianProcess(object):
         self.lengthscale = lengthscale
         self.variance = variance
         self.plot_x = np.arange(-60, 60, 0.1)
-    
 
 
     def Cal_kernel(self, X1, X2):
@@ -21,7 +20,6 @@ class GaussianProcess(object):
                     (2 * self.alpha * (self.lengthscale ** 2)))
                 Kernel[iter_y][iter_x] = (self.variance ** 2) * (fucking ** (- self.alpha))
         return Kernel
-    
 
 
     def opt_Cal_kernel(self, X1, X2, alpha , variance, lengthscale):
@@ -31,12 +29,9 @@ class GaussianProcess(object):
                 fucking = 1 + (((X1[iter_y] - X2[iter_x]) ** 2) / \
                     (2 * alpha * (lengthscale ** 2)))
                 Kernel[iter_y][iter_x] = (variance ** 2) * (fucking ** (- alpha))
-        return Kernel    
-    #def Cal_small_kernel(self, X1, X2, alpha = 1, variance = 1, lengthscale = 1):
-    #    fucking = 1 + (((X1[iter_y] - X2) ** 2) / (2 * alpha * (lengthscale ** 2)))
-    #    return (variance ** 2) * (fucking ** (- alpha))
-    
-    
+        return Kernel
+
+
     def get_data(self, filename = "input.data"):
         list_x = list()
         list_y = list()
@@ -55,33 +50,22 @@ class GaussianProcess(object):
         self.y = np.asarray(list_y)
     
         return self.x, self.y
-        
-    
-    
+
+
     def Cal_mean(self, x, y):
         sample = np.arange(-60, 60, 0.1)
-        ## cal K_X_Xstar
+
         self.Kernel_xn_xm = self.Cal_kernel(X1 = x, X2 = x)
         self.C_xn_xm = self.Kernel_xn_xm + (1 / self.beta * self.delta)
         self.K_X_Xstar = self.Cal_kernel(X1 = x, X2 = self.plot_x)
-        #K_Xstar_Xstar = self.Cal_kernel(X1 = sample, X2 = sample) + 1 / self.beta
 
         mean = self.K_X_Xstar.T @ (np.linalg.inv(self.C_xn_xm)) @ y
         return mean
 
 
-
-
     def Cal_var(self, x, y):
         K_Xstar_Xstar = np.zeros(len(self.plot_x))
         variance = np.zeros(len(self.plot_x))
-        print("variance", variance.shape)
-        print("K_Xstar_Xstar", K_Xstar_Xstar.shape)
-        print("K_X_Xstar", self.K_X_Xstar.shape)
-        print("self.C_xn_xm", self.C_xn_xm.shape)
-
-        print("test self.K_X_Xstar[:,iter_pixel]", self.K_X_Xstar[:,1].shape)
-
 
         for iter_pixel in range(len(K_Xstar_Xstar)):
             fucking = 1 + (((self.plot_x[iter_pixel] - self.plot_x[iter_pixel]) ** 2) / \
@@ -91,23 +75,18 @@ class GaussianProcess(object):
         return variance
 
 
-
-
-
     def Log_Likelihood(self, x0):
-        #self.Kernel_xn_xm
         alpha, lengthscale, variance = x0[0], x0[1], x0[2]
-        #alpha, lengthscale = x0[0], x0[1]
         K = self.opt_Cal_kernel(X1 = self.x, X2 = self.x, alpha = alpha, lengthscale = lengthscale, variance = variance)
-        if np.linalg.matrix_rank(K) != len(self.x):
-            return 1000
-        #K = self.Kernel_xn_xm
         lgggg = np.abs(np.linalg.det(K))
-        if lgggg == 0:
+        if np.linalg.matrix_rank(K) != len(self.x) or lgggg == 0:
             return 1000
+        
         lg_norm_K = np.log(lgggg)
         inv_K = np.linalg.inv(K)
         ans = 0.5 * (self.y.T @ inv_K @ self.y + lg_norm_K + len(self.x) * np.log(2 * np.pi))
+        if ans < 0.001:
+            return 1000
         print(ans)
         return ans
 
@@ -116,59 +95,56 @@ class GaussianProcess(object):
     def optimize(self):
         error = 1000
         inits = np.arange(1, 10, 1)
-        #opt_alpha, opt_lengthscale, opt_variance = 1.0, 1.0, 1.0
         opt_alpha, opt_lengthscale = 1.0, 1.0
         for iter_alpha in inits:
             for iter_lengthscale in inits:
                 for iter_variance in inits:
                     result = minimize(self.Log_Likelihood, x0 = [iter_alpha, iter_lengthscale, iter_variance], method = "SLSQP")
-                    #result = minimize(self.Log_Likelihood, x0 = [iter_alpha, iter_lengthscale], method = "SLSQP")
                     if result.fun < error:
                         error = result.fun
-                        #opt_alpha, opt_lengthscale = result.x
                         opt_alpha, opt_lengthscale, opt_variance = result.x
         return opt_alpha, opt_lengthscale, opt_variance, error
 
-
-
-
-
-
-    def plotting(self, mean, variance, x, y):
-        plt.title("Gaussian Process Regression")
-        
-        plt.plot(self.plot_x, mean, 'coral')
-        vvvvvvar = self.funCtion(x = self.plot_x, variance = variance)
-        plt.fill_between(self.plot_x, mean - vvvvvvar, mean + vvvvvvar, color='aqua')
-        plt.scatter(x, y, c = "black")
-
-        plt.show()
     
-    
-    def funCtion(self, x, variance):
-        var = np.zeros(len(variance))
-        for iter_element in range(len(variance)):
-            if variance[iter_element] == 0:
-                print("fucking 0")
-            var[iter_element] = 2 * (variance[iter_element] ** 0.5)
-        return var
-    
-    
-    def print_Kernel(self, Kernel):
-        row, column = Kernel.shape
-        for iter_y in range(column):
-            for iter_x in range(row):
-                print(Kernel[iter_y][iter_x], end = " ")
-            print()
+def funCtion(x, variance):
+    var = np.zeros(len(variance))
+    for iter_element in range(len(variance)):
+        if variance[iter_element] == 0:
+            print("fucking 0")
+        var[iter_element] = 2 * (variance[iter_element] ** 0.5)
+    return var
 
 
+def plotting(mean, variance, opt_mean, opt_variance, para, x, y):
+    plot_x = np.arange(-60, 60, 0.1)
+    plt.subplot(121)
+    plt.title("Gaussian Process Regression")
+    plt.plot(plot_x, mean, 'coral')
+    var = funCtion(x = plot_x, variance = variance)
+    plt.fill_between(plot_x, mean - var, mean + var, color='aqua')
+    plt.scatter(x, y, c = "black")
+
+    plt.subplot(122)
+    plt.title("Gaussian Process Regression (Optimized)")
+    plt.plot(plot_x, opt_mean, 'coral')
+    opt_var = funCtion(x = plot_x, variance = opt_variance)
+    plt.fill_between(plot_x, mean - opt_var, mean + opt_var, color='aqua')
+    plt.scatter(x, y, c = "black")
+    annotated = "alpha : " + para[0] + "\nlengthscale : " + para[1] + "\nvariance : " + para[2]
+    plt.text(0.5, -6.1, annotated, size=10, rotation=30.,ha="center", va="bottom",bbox=dict(boxstyle="round",ec=(1., 0.5, 0.5),fc=(1., 0.8, 0.8),))
+
+    plt.show() 
+    
+def print_Kernel(Kernel):
+    row, column = Kernel.shape
+    for iter_y in range(column):
+        for iter_x in range(row):
+            print(Kernel[iter_y][iter_x], end = " ")
+        print()
 
 
 ## result
-## 1335.1519158971716 -1.1007435869267364 34164.09859126492
-## 5.690400937090753 2.0249092985716213
-
-
+## 8.491449330946992 -2.4761190591796756 1.37230728866543
 
 
 ## reference
--- hw5/hw5.py
+++ hw5/hw5.py
@@ -4,51 +4,25 @@ from GaussianProcess import *
 
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument("--alpha", type = float, default =  1.0, help = "alpha")
 parser.add_argument("--lengthscale", type = float, default =  1.0, help = "lengthscale")
 parser.add_argument("--variance", type = float, default = 1.0, help = "variance")
-
 input_ = parser.parse_args()
 
-print("para", input_.alpha, input_.lengthscale, input_.variance)
-
 GP = GaussianProcess(alpha = input_.alpha, lengthscale = input_.lengthscale, variance = input_.variance)
-
-
 x, y = GP.get_data()
-
-
-
-#print_Kernel(Kernel)
-
-#sample = np.arange(-60, 60, 0.01)
-#
-#mean = list()
-#
-#var = list()
-#
-#K_X_Xstar = Cal_kernel(X1 = x, X2 = x)
-
-
-#test = GP.Cal_kernel(x, x)
-
-#GP.print_Kernel(Kernel = test)
 
 mean = GP.Cal_mean(x = x, y = y)
 variance = GP.Cal_var(x = x, y = y)
 
 
-#print(GP.Cal_kernel(4, 3))
+opt_alpha, opt_lengthscale, opt_variance, error = GP.optimize()
 
-GP.plotting(mean, variance, x, y)
+opt_GP = GaussianProcess(alpha = opt_alpha, lengthscale = opt_lengthscale, variance = opt_variance)
 
-print(x, y)
+opt_mean = opt_GP.Cal_mean(x = x, y = y)
+opt_variance = opt_GP.Cal_var(x = x, y = y)
 
-#opt_alpha, opt_lengthscale, opt_variance, error = GP.optimize()
-#opt_alpha, opt_lengthscale = GP.optimize()
 
-#print(opt_alpha, opt_lengthscale)
-
-#print(opt_alpha, opt_lengthscale, opt_variance, error)
-
+para = [str(opt_alpha), str(opt_lengthscale), str(opt_variance)]
+plotting(mean, variance, opt_mean, opt_variance, para, x, y)
