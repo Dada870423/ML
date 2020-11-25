5f11fa0bf4904ca2545be6154d6c1af9a6ad140b
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-11-25 14:09:37 +0800

    norm every 10 times

--- hw5_2/hw5_2.py
+++ hw5_2/hw5_2.py
@@ -3,3 +3,12 @@ import numpy as np
 from SVM import *
 
 mode = input("your mode")
+
+if mode == 0:
+	print("comparison")
+elif mode == 1:
+	print("C-SVC")
+elif mode == 2:
+	print("user-defined kernel")
+else:
+	print("input the valid mode")
