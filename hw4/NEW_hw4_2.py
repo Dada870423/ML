1229d13d742011913f44a58b0528c22cfb35d410
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-11-26 13:26:51 +0800

    60000

--- hw4/NEW_hw4_2.py
+++ hw4/NEW_hw4_2.py
@@ -10,7 +10,7 @@ from New_EM import *
 train_label_file = "file/train-labels-idx1-ubyte"
 train_image_file = "file/train-images-idx3-ubyte"
 
-input_N = 1000
+input_N = 60000
 lamBda = np.ones(10)
 probability = np.random.rand(28 * 28, 10)
 hidden_W = np.ones((input_N, 10))
@@ -32,7 +32,36 @@ print(hidden_W[4])
 print(hidden_W[5])
 print("lambda", lamBda)
 
+E_step(Binomial_matrix, input_N = input_N, lamBda = lamBda, probability = probability, hidden_W = hidden_W)
+M_step(Binomial_matrix, lamBda, hidden_W, probability, input_N)
+print(hidden_W[3])
+print(hidden_W[4])
+print(hidden_W[5])
+print("lambda", lamBda)
 
+
+E_step(Binomial_matrix, input_N = input_N, lamBda = lamBda, probability = probability, hidden_W = hidden_W)
+M_step(Binomial_matrix, lamBda, hidden_W, probability, input_N)
+print(hidden_W[3])
+print(hidden_W[4])
+print(hidden_W[5])
+print("lambda", lamBda)
+
+
+E_step(Binomial_matrix, input_N = input_N, lamBda = lamBda, probability = probability, hidden_W = hidden_W)
+M_step(Binomial_matrix, lamBda, hidden_W, probability, input_N)
+print(hidden_W[3])
+print(hidden_W[4])
+print(hidden_W[5])
+print("lambda", lamBda)
+
+
+E_step(Binomial_matrix, input_N = input_N, lamBda = lamBda, probability = probability, hidden_W = hidden_W)
+M_step(Binomial_matrix, lamBda, hidden_W, probability, input_N)
+print(hidden_W[3])
+print(hidden_W[4])
+print(hidden_W[5])
+print("lambda", lamBda)
 
 GroundTruth = Test(Binomial_matrix, Label_fptr, label, probability)
 
