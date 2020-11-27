d0e68d59fecf76a11efb4e28eb303896aa047951
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-11-27 15:36:09 +0800

    hw5 init svm

---
+++ hw5_2/SVM.py
@@ -0,0 +1,73 @@
+import numpy as np
+import sys
+import csv
+from libsvm.svm import *
+from libsvm import svmutil
+#from libsvm.python.svmutil import *
+#from libsvm.python.svm import *
+
+
+class SupportVectorMachine():
+    def __init__(self, mode):
+        self.mode = mode
+        self.input_size = 5000
+        #self.TrainImage = np.zeros((self.input_size, 784))
+
+
+    def ReadTrainingFile(self, file):
+        image_ptr = open(file[0], "r")
+        test_list = list(csv.reader(image_ptr))
+        list_of_floats = [float(item) for a_list in  test_list  for item in a_list]
+        self.TrainImage = np.array(list_of_floats).reshape((5000, 784))
+        #print(haha)
+
+        label_ptr = open(file[1], "r")
+        test_list = list(csv.reader(label_ptr))
+        list_of_floats = [int(item) for a_list in  test_list  for item in a_list]
+        self.TrainLabel = np.array(list_of_floats)
+
+
+
+        #self.TrainImage = haha
+
+        #image_ptr = open(file[1], "r")
+
+    def ReadTestFile(self, file):
+        image_ptr = open(file[0], "r")
+        test_list = list(csv.reader(image_ptr))
+        print(np.size(test_list))
+        list_of_floats = [float(item) for a_list in  test_list  for item in a_list]
+        self.TestImage = np.array(list_of_floats).reshape((2500, 784))
+
+        label_ptr = open(file[1], "r")
+        test_list = list(csv.reader(label_ptr))
+        list_of_floats = [int(item) for a_list in  test_list  for item in a_list]
+        self.TestLabel = np.array(list_of_floats)
+
+
+    def RUN(self):
+        result = 0
+        if self.mode == 0:
+            result = self.compare()
+        return result
+
+    def compare(self):
+        m = svm_train(self.TrainLabel, self.TrainImage, f'-t 1 -d 2 -q')
+        res = svm_predict(self.TestLabel, self.TestImage, m)
+        return res
+
+
+
+
+
+    def print_image(self, number):
+        for i in range(28):
+            for j in range(28):
+                print(int(self.TrainImage[number][i*28 + j] > 0.5), end = " ")
+            print()
+        for i in range(30):
+            print(self.TrainLabel[i], end = " ")
+
+
+
+
