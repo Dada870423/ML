d2b59a577d46ca12ef66b3b18a87cba223ee673b
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-11-27 14:42:20 +0800

    init hw5)2

--- hw5_2/hw5_2.py
+++ hw5_2/hw5_2.py
@@ -2,13 +2,32 @@ import numpy as np
 
 from SVM import *
 
-mode = input("your mode")
+mode = int(input("your mode :  "))
+
+file = ["X_train.csv​", "Y_train.csv​"]
+
+print("X_train.csv​")
+
 
 if mode == 0:
-	print("comparison")
+    print("comparison")
 elif mode == 1:
-	print("C-SVC")
+    print("C-SVC")
 elif mode == 2:
-	print("user-defined kernel")
+    print("user-defined kernel")
 else:
-	print("input the valid mode")
+    print("input the valid mode")
+    exit(0)
+
+svm = SupportVectorMachine(mode = mode)
+
+svm.ReadTrainingFile(file = ["X_train.csv", "Y_train.csv"])
+
+svm.ReadTestFile(file = ["X_test.csv", "Y_test.csv"])
+
+svm.RUN()
+
+
+svm.print_image(4)
+
+
