f745a77f5e1cfaee0508e366bea83c6535069b98
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-12-14 16:33:23 +0800

    color output

--- hw6/SPECTRAL.py
+++ hw6/SPECTRAL.py
@@ -11,18 +11,17 @@ def Spectral(Gram, k, mode = 1):
 
     ## compute the first k eigenvectors
 
-    saved_vectors_sort = "./T_vector_S_45_C_32_sort.npy"
-    saved_values_sort = "./T_value_S_45_C_32_sort.npy"
+    saved_vectors_sort = "./T_vector_S_65_C_62_sort.npy"
+    saved_values_sort = "./T_value_S_65_C_62_sort.npy"
     if not os.path.isfile(saved_vectors_sort):
         # degree matrix
-        Degree = np.diag(np.sum(Gram, axis=1))
+        Degree = np.sum(Gram, axis=1)
         Laplacian = Degree - Gram
-        D__1_2 = np.diag(1 / np.sqrt(Degree[Degree!=0]))
+        D__1_2 = np.diag(Degree ** (-0.5))
         Laplacian_sym = D__1_2 @ Laplacian @ D__1_2
 
         print("no file")
-        #eigen_values_unsorted, eigen_vectors_unsorted = np.linalg.eig(Laplacian_sym)
-        Eigen = linalg.eig(Laplacian_sym)
+        Eigen = linalg.eigh(Laplacian_sym)
         eigen_values_unsorted = Eigen[0]
         eigen_vectors_unsorted = Eigen[1]
 
@@ -40,19 +39,8 @@ def Spectral(Gram, k, mode = 1):
 
 
     ## normalize
-    #Eigen_Vector_T = np.zeros((Eigen_Vector.shape))
 
     Eigen_Vector_trans = Eigen_Vector[0:k].T
-
-    #print(Eigen_Vector_trans.shape)
-    #eigen_test = Eigen_Vector_trans.T
-
-    print(Eigen_Vector_trans.shape)
-
-    print(Eigen_Vector_trans[0])
-
-
-
     for iter_row in range(len(Eigen_Vector_trans)):
         SSum = np.sum(Eigen_Vector_trans[iter_row])
         Eigen_Vector_trans[iter_row] = Eigen_Vector_trans[iter_row] / SSum
@@ -60,26 +48,16 @@ def Spectral(Gram, k, mode = 1):
     #GIF = Kmeans(Gram = Eigen_Vector_T, k = k)
     print(Eigen_Vector_trans[0])
 
-    Gram_spectral = Spectral_kernel(Eigen_Vector = Eigen_Vector_trans, Gamma = 0.00001)
-
+    Gram_spectral = Spectral_kernel(Eigen_Vector = Eigen_Vector_trans, Gamma = 0.001)
     GIF = Kmeans(Gram = Gram_spectral, k = k, mode = mode)
-
-
 
     return GIF
 
 
 def Spectral_kernel(Eigen_Vector, Gamma):
-
-
     Dis_kernel = (- Gamma * pdist(Eigen_Vector, 'sqeuclidean'))
-
     Kernelone = np.exp(Dis_kernel)
-
     Kernel = squareform(Kernelone)
-    #print("one", Kernel.shape)
-    #for i in range(len(Kernelone)):
-    #    print(Kernelone[i], end = " ")
 
     return Kernel
 
--- hw6/hw6.py
+++ hw6/hw6.py
@@ -7,8 +7,8 @@ from KMEANS import *
 from SPECTRAL import *
 
 file = "./image1.png"
-Gamma_s = 0.0000001 ## for spatial
-Gamma_c = 0.0002 ## for color
+Gamma_s = 0.000005 ## for spatial
+Gamma_c = 0.000002 ## for color
 
 image = Read_image(file = file) ## read file
 
@@ -16,9 +16,42 @@ image = Read_image(file = file) ## read file
 Gram = PreComputed_kernel(image = image, Gamma_s = Gamma_s, Gamma_c = Gamma_c) 
 
 ## Using Kmeans clustering
-GIF_Kmeans = Kmeans(Gram = Gram, k = 5, mode = 1)
+GIF_Kmeans = Kmeans(Gram = Gram, k = 2, mode = 1)
 ## Using Spectral clustering
-GIF_spectral = Spectral(Gram = Gram, k = 5, mode = 1)
+GIF_spectral = Spectral(Gram = Gram, k = 2, mode = 0)
 ## Save Gif
-imageio.mimsave("Spectral_eric.gif", GIF_Kmeans)
-imageio.mimsave("Spectral_eric.gif", GIF_spectral)
+
+
+color_list = [[0, 0, 0], [255, 255, 255], [255, 0, 0], [0, 255, 0], [0, 0, 255], [255, 255, 0], [255, 0, 255], [0, 255, 255]]
+
+m, n, p = GIF_Kmeans.shape
+
+
+GIF_Kmeans_output = []
+
+for iter_m in range(m):
+    GIF_Kmeans_output.appen([])
+    for iter_n in range(n):
+        for iter_p in range(p):
+            GIF_Kmeans_output[m].append(color_list[int(GIF_Kmeans[m][n][p])])
+
+GIF_Kmeans_output = np.array(GIF_Kmeans_output).reshape((m, 100, 100, 3))
+
+m, n, p = GIF_spectral.shape
+
+
+GIF_spectral_output = []
+
+for iter_m in range(m):
+    GIF_spectral_output.appen([])
+    for iter_n in range(n):
+        for iter_p in range(p):
+            GIF_spectral_output[m].append(color_list[int(GIF_spectral_output[m][n][p])])
+
+GIF_spectral_output = np.array(GIF_spectral_output).reshape((m, 100, 100, 3))
+
+
+
+
+imageio.mimsave("Spectral_sean.gif", GIF_Kmeans_output)
+imageio.mimsave("Spectral_sean.gif", GIF_spectral_output)
